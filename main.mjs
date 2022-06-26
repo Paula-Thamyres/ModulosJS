a7db3315ea3fbe3155b8b65809629056551763b6
Paula Thamyres da Silva <84049116+Paula-Thamyres@users.noreply.github.com>
2022-06-26 19:16:01 -0300

    Módulos em Javascript

---
+++ main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade("Paula", 32));
+
+console.log(mostraCidade("Paula", 'Araras-SP'));
+
+console.log(mostraHobby("Paula", "Maratonar Series"));
